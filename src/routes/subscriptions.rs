4bf8eb8b6a2eba23f69057f0aaa3326ad47ea447
Edward Snow <edward.snow@disneystreaming.com>
2024-10-04 12:03:35 +0100

    Split functions and add intrumentation via proc macros

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,6 @@ use sqlx::PgPool;
 use uuid::Uuid;
 use chrono::Utc;
 use tracing;
-use tracing_futures::Instrument;
 
 // This macro automatically implements the correct serialise/deserialise implementation for us
 //  In our case, it'll parse the definition of the FormData type and generate the right implementation for it
@@ -13,37 +12,36 @@ pub struct FormData {
     pub name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieving a pool from the application state!
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    // Spans, like logs, have an associated level
-    // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        // This is implicitly providing a key=value pair by using the variable name as the key
-        // The % is used to tell tracing to use their Display implementation for logging purposes.
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    
-    // _request_span_guard is dropped at the end of `subscribe`. That's when we "exit" the span.
-    let _request_span_guard = request_span.enter();
-    
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the query future lifetime.
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database"
-    );
+    match insert_subscriber(&pool, &form).await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
 
-    // `Result` has two variants: `Ok` and `Err`.
-    // The first for successes, the second for failures.
-    // We use a `match` statement to choose what to do based on the outcome.
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -53,18 +51,12 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-        // We use `get_ref` to get an immutable reference to the `PgConnection` wrapped by `web::Data`.
-        .execute(pool.get_ref())
-        // This passes the span to the future and we'll see "query_span" info log every time the executor polls the future
-        .instrument(query_span)
+        .execute(pool)
         .await
-    {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
+        .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+            e
+        // Using the `?` operator to return early if the function failed, returning a sqlx::Error
+        })?;
+        Ok(())
 }

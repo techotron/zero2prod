37e1d950e4c618932c64f55103becebe05ebf33b
Edward Snow <edward.snow@disneystreaming.com>
2024-10-04 12:11:54 +0100

    Use actix tracing middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,19 @@
 use crate::{routes::health_check, routes::subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
